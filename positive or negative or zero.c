6f87130fb3bab30798773b7eba8cbe5a139570b4
vinothkarbonn <vinoth.vece@ksrce.ac.in>
2017-06-05 14:16:34 +0530

    positive or negative or zero.c

---
+++ positive or negative or zero.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+if(n>1||n!=0){
+printf("the number is negative");}
+elseif(n<1||n!=0){
+printf("the number is positive");}
+else{
+printf(the number is zero");}
+getch();
+}
