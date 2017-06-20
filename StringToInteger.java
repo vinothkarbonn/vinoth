b3675dadd28d47a678fb92176fb98197d4f1f04d
vinothkarbonn <vinoth.vece@ksrce.ac.in>
2017-06-20 09:19:30 +0530

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,10 @@
+
+public class StringToInteger {
+	public static void main(String[] args){
+		String number = "10";
+		int result = Integer.parseInt(number);
+		System.out.println(result);
+		
+	}
+
+}
