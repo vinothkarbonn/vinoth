4dee99074c96dad481dfe557389438551cef4458
vinothkarbonn <vinoth.vece@ksrce.ac.in>
2017-07-17 12:39:49 +0530

    Create longest.java

---
+++ longest.java
@@ -0,0 +1,24 @@
+class alpha
+{
+public static void main(String[] args)
+{
+String a=args[0];
+String b="",c="",d="",e=" ";
+int i=0,k=0;
+for(i=0;i<=a.length()-2;i++)
+{
+for(k=a.length();k>i;k--){
+b=a.substring(i,k);
+c=new StringBuffer(b).reverse().toString();
+if(b.equals(c))
+{
+d=b;
+if(d.length()>e.length())
+{System.out.println(e);
+e=d;}
+}
+}
+}
+System.out.println(e);
+}
+}
