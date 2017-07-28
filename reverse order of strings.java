9505a47109db269e4e6d50a0b01962e7f5e1973c
vinothkarbonn <vinoth.vece@ksrce.ac.in>
2017-07-28 14:49:40 +0530

    Add files via upload

---
+++ reverse order of strings.java
@@ -0,0 +1,13 @@
+package assignment08;
+
+public class Vinoth
+{
+	public static void main(String[] args) {
+		String str=new String("VINOTH");
+		String[] str2=str.split(" ");
+		for(int m=str2.length-1;m>=0;m--)
+		{
+                  System.out.print(str2[m]+" ");
+			 }
+	}
+}
