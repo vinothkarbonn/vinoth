d2215b1791393193d6ad21b7b86d3767165e476f
vinothkarbonn <vinoth.vece@ksrce.ac.in>
2017-07-28 14:26:20 +0530

    Create reverse words.java

---
+++ reverse words.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hello{
+public static void main(String[] args){
+Scanner s=new Scanner(System.in);
+String str=s.newLine();
+String str1[]=str.split(" ");
+String str2=" ";
+for(int i=Str1.length;i>=0;i--){
+str2+=str1[i];
+}
+System.out.println(str2);
+}
+}
